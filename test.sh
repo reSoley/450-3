0b6b345e480ccd45a842586d169df0a5b4892d7c
Tim Lagnese <timlag1305@gmail.com>
2017-04-03 21:40:11 -0400

    Add test file for merge sort

---
+++ test.sh
@@ -0,0 +1,542 @@
+#! /bin/bash
+# SEQUENTIAL MERGE SORT
+# run the first test 10 times
+./mergesort 10000000 0 0 64
+./mergesort 10000000 0 0 64
+./mergesort 10000000 0 0 64
+./mergesort 10000000 0 0 64
+./mergesort 10000000 0 0 64
+./mergesort 10000000 0 0 64
+./mergesort 10000000 0 0 64
+./mergesort 10000000 0 0 64
+./mergesort 10000000 0 0 64
+./mergesort 10000000 0 0 64
+
+# 2nd test
+./mergesort 100000000 0 0 64
+./mergesort 100000000 0 0 64
+./mergesort 100000000 0 0 64
+./mergesort 100000000 0 0 64
+./mergesort 100000000 0 0 64
+./mergesort 100000000 0 0 64
+./mergesort 100000000 0 0 64
+./mergesort 100000000 0 0 64
+./mergesort 100000000 0 0 64
+./mergesort 100000000 0 0 64
+
+# 3rd test
+./mergesort 1000000000 0 0 64
+./mergesort 1000000000 0 0 64
+./mergesort 1000000000 0 0 64
+./mergesort 1000000000 0 0 64
+./mergesort 1000000000 0 0 64
+./mergesort 1000000000 0 0 64
+./mergesort 1000000000 0 0 64
+./mergesort 1000000000 0 0 64
+./mergesort 1000000000 0 0 64
+./mergesort 1000000000 0 0 64
+
+# 4th test
+./mergesort 10000000 1 0 64
+./mergesort 10000000 1 0 64
+./mergesort 10000000 1 0 64
+./mergesort 10000000 1 0 64
+./mergesort 10000000 1 0 64
+./mergesort 10000000 1 0 64
+./mergesort 10000000 1 0 64
+./mergesort 10000000 1 0 64
+./mergesort 10000000 1 0 64
+./mergesort 10000000 1 0 64
+
+# 5th test
+./mergesort 100000000 1 0 64
+./mergesort 100000000 1 0 64
+./mergesort 100000000 1 0 64
+./mergesort 100000000 1 0 64
+./mergesort 100000000 1 0 64
+./mergesort 100000000 1 0 64
+./mergesort 100000000 1 0 64
+./mergesort 100000000 1 0 64
+./mergesort 100000000 1 0 64
+./mergesort 100000000 1 0 64
+
+# 6th test
+./mergesort 1000000000 1 0 64
+./mergesort 1000000000 1 0 64
+./mergesort 1000000000 1 0 64
+./mergesort 1000000000 1 0 64
+./mergesort 1000000000 1 0 64
+./mergesort 1000000000 1 0 64
+./mergesort 1000000000 1 0 64
+./mergesort 1000000000 1 0 64
+./mergesort 1000000000 1 0 64
+./mergesort 1000000000 1 0 64
+
+# 7th test
+./mergesort 10000000 2 0 64
+./mergesort 10000000 2 0 64
+./mergesort 10000000 2 0 64
+./mergesort 10000000 2 0 64
+./mergesort 10000000 2 0 64
+./mergesort 10000000 2 0 64
+./mergesort 10000000 2 0 64
+./mergesort 10000000 2 0 64
+./mergesort 10000000 2 0 64
+./mergesort 10000000 2 0 64
+
+# 8th test
+./mergesort 100000000 2 0 64
+./mergesort 100000000 2 0 64
+./mergesort 100000000 2 0 64
+./mergesort 100000000 2 0 64
+./mergesort 100000000 2 0 64
+./mergesort 100000000 2 0 64
+./mergesort 100000000 2 0 64
+./mergesort 100000000 2 0 64
+./mergesort 100000000 2 0 64
+./mergesort 100000000 2 0 64
+
+# 9th test
+./mergesort 1000000000 2 0 64
+./mergesort 1000000000 2 0 64
+./mergesort 1000000000 2 0 64
+./mergesort 1000000000 2 0 64
+./mergesort 1000000000 2 0 64
+./mergesort 1000000000 2 0 64
+./mergesort 1000000000 2 0 64
+./mergesort 1000000000 2 0 64
+./mergesort 1000000000 2 0 64
+./mergesort 1000000000 2 0 64
+
+# 10th test
+./mergesort 10000000 3 0 64
+./mergesort 10000000 3 0 64
+./mergesort 10000000 3 0 64
+./mergesort 10000000 3 0 64
+./mergesort 10000000 3 0 64
+./mergesort 10000000 3 0 64
+./mergesort 10000000 3 0 64
+./mergesort 10000000 3 0 64
+./mergesort 10000000 3 0 64
+./mergesort 10000000 3 0 64
+
+# 11th test
+./mergesort 100000000 3 0 64
+./mergesort 100000000 3 0 64
+./mergesort 100000000 3 0 64
+./mergesort 100000000 3 0 64
+./mergesort 100000000 3 0 64
+./mergesort 100000000 3 0 64
+./mergesort 100000000 3 0 64
+./mergesort 100000000 3 0 64
+./mergesort 100000000 3 0 64
+./mergesort 100000000 3 0 64
+
+# 12th test
+./mergesort 1000000000 3 0 64
+./mergesort 1000000000 3 0 64
+./mergesort 1000000000 3 0 64
+./mergesort 1000000000 3 0 64
+./mergesort 1000000000 3 0 64
+./mergesort 1000000000 3 0 64
+./mergesort 1000000000 3 0 64
+./mergesort 1000000000 3 0 64
+./mergesort 1000000000 3 0 64
+
+# 13th test
+./mergesort 10000000 4 0 64
+./mergesort 10000000 4 0 64
+./mergesort 10000000 4 0 64
+./mergesort 10000000 4 0 64
+./mergesort 10000000 4 0 64
+./mergesort 10000000 4 0 64
+./mergesort 10000000 4 0 64
+./mergesort 10000000 4 0 64
+./mergesort 10000000 4 0 64
+./mergesort 10000000 4 0 64
+
+# 14th test
+./mergesort 100000000 4 0 64
+./mergesort 100000000 4 0 64
+./mergesort 100000000 4 0 64
+./mergesort 100000000 4 0 64
+./mergesort 100000000 4 0 64
+./mergesort 100000000 4 0 64
+./mergesort 100000000 4 0 64
+./mergesort 100000000 4 0 64
+./mergesort 100000000 4 0 64
+./mergesort 100000000 4 0 64
+
+# 15th test
+./mergesort 1000000000 4 0 64
+./mergesort 1000000000 4 0 64
+./mergesort 1000000000 4 0 64
+./mergesort 1000000000 4 0 64
+./mergesort 1000000000 4 0 64
+./mergesort 1000000000 4 0 64
+./mergesort 1000000000 4 0 64
+./mergesort 1000000000 4 0 64
+./mergesort 1000000000 4 0 64
+./mergesort 1000000000 4 0 64
+
+# PARALLEL MERGE SORT
+# run the first test 10 times
+./mergesort 10000000 0 1 64
+./mergesort 10000000 0 1 64
+./mergesort 10000000 0 1 64
+./mergesort 10000000 0 1 64
+./mergesort 10000000 0 1 64
+./mergesort 10000000 0 1 64
+./mergesort 10000000 0 1 64
+./mergesort 10000000 0 1 64
+./mergesort 10000000 0 1 64
+./mergesort 10000000 0 1 64
+
+# 2nd test
+./mergesort 100000000 0 1 64
+./mergesort 100000000 0 1 64
+./mergesort 100000000 0 1 64
+./mergesort 100000000 0 1 64
+./mergesort 100000000 0 1 64
+./mergesort 100000000 0 1 64
+./mergesort 100000000 0 1 64
+./mergesort 100000000 0 1 64
+./mergesort 100000000 0 1 64
+./mergesort 100000000 0 1 64
+
+# 3rd test
+./mergesort 1000000000 0 1 64
+./mergesort 1000000000 0 1 64
+./mergesort 1000000000 0 1 64
+./mergesort 1000000000 0 1 64
+./mergesort 1000000000 0 1 64
+./mergesort 1000000000 0 1 64
+./mergesort 1000000000 0 1 64
+./mergesort 1000000000 0 1 64
+./mergesort 1000000000 0 1 64
+./mergesort 1000000000 0 1 64
+
+# 4th test
+./mergesort 10000000 1 1 64
+./mergesort 10000000 1 1 64
+./mergesort 10000000 1 1 64
+./mergesort 10000000 1 1 64
+./mergesort 10000000 1 1 64
+./mergesort 10000000 1 1 64
+./mergesort 10000000 1 1 64
+./mergesort 10000000 1 1 64
+./mergesort 10000000 1 1 64
+./mergesort 10000000 1 1 64
+
+# 5th test
+./mergesort 100000000 1 1 64
+./mergesort 100000000 1 1 64
+./mergesort 100000000 1 1 64
+./mergesort 100000000 1 1 64
+./mergesort 100000000 1 1 64
+./mergesort 100000000 1 1 64
+./mergesort 100000000 1 1 64
+./mergesort 100000000 1 1 64
+./mergesort 100000000 1 1 64
+./mergesort 100000000 1 1 64
+
+# 6th test
+./mergesort 1000000000 1 1 64
+./mergesort 1000000000 1 1 64
+./mergesort 1000000000 1 1 64
+./mergesort 1000000000 1 1 64
+./mergesort 1000000000 1 1 64
+./mergesort 1000000000 1 1 64
+./mergesort 1000000000 1 1 64
+./mergesort 1000000000 1 1 64
+./mergesort 1000000000 1 1 64
+./mergesort 1000000000 1 1 64
+
+# 7th test
+./mergesort 10000000 2 1 64
+./mergesort 10000000 2 1 64
+./mergesort 10000000 2 1 64
+./mergesort 10000000 2 1 64
+./mergesort 10000000 2 1 64
+./mergesort 10000000 2 1 64
+./mergesort 10000000 2 1 64
+./mergesort 10000000 2 1 64
+./mergesort 10000000 2 1 64
+./mergesort 10000000 2 1 64
+
+# 8th test
+./mergesort 100000000 2 1 64
+./mergesort 100000000 2 1 64
+./mergesort 100000000 2 1 64
+./mergesort 100000000 2 1 64
+./mergesort 100000000 2 1 64
+./mergesort 100000000 2 1 64
+./mergesort 100000000 2 1 64
+./mergesort 100000000 2 1 64
+./mergesort 100000000 2 1 64
+./mergesort 100000000 2 1 64
+
+# 9th test
+./mergesort 1000000000 2 1 64
+./mergesort 1000000000 2 1 64
+./mergesort 1000000000 2 1 64
+./mergesort 1000000000 2 1 64
+./mergesort 1000000000 2 1 64
+./mergesort 1000000000 2 1 64
+./mergesort 1000000000 2 1 64
+./mergesort 1000000000 2 1 64
+./mergesort 1000000000 2 1 64
+./mergesort 1000000000 2 1 64
+
+# 10th test
+./mergesort 10000000 3 1 64
+./mergesort 10000000 3 1 64
+./mergesort 10000000 3 1 64
+./mergesort 10000000 3 1 64
+./mergesort 10000000 3 1 64
+./mergesort 10000000 3 1 64
+./mergesort 10000000 3 1 64
+./mergesort 10000000 3 1 64
+./mergesort 10000000 3 1 64
+./mergesort 10000000 3 1 64
+
+# 11th test
+./mergesort 100000000 3 1 64
+./mergesort 100000000 3 1 64
+./mergesort 100000000 3 1 64
+./mergesort 100000000 3 1 64
+./mergesort 100000000 3 1 64
+./mergesort 100000000 3 1 64
+./mergesort 100000000 3 1 64
+./mergesort 100000000 3 1 64
+./mergesort 100000000 3 1 64
+./mergesort 100000000 3 1 64
+
+# 12th test
+./mergesort 1000000000 3 1 64
+./mergesort 1000000000 3 1 64
+./mergesort 1000000000 3 1 64
+./mergesort 1000000000 3 1 64
+./mergesort 1000000000 3 1 64
+./mergesort 1000000000 3 1 64
+./mergesort 1000000000 3 1 64
+./mergesort 1000000000 3 1 64
+./mergesort 1000000000 3 1 64
+./mergesort 1000000000 3 1 64
+
+# 13th test
+./mergesort 10000000 4 1 64
+./mergesort 10000000 4 1 64
+./mergesort 10000000 4 1 64
+./mergesort 10000000 4 1 64
+./mergesort 10000000 4 1 64
+./mergesort 10000000 4 1 64
+./mergesort 10000000 4 1 64
+./mergesort 10000000 4 1 64
+./mergesort 10000000 4 1 64
+./mergesort 10000000 4 1 64
+
+# 14th test
+./mergesort 100000000 4 1 64
+./mergesort 100000000 4 1 64
+./mergesort 100000000 4 1 64
+./mergesort 100000000 4 1 64
+./mergesort 100000000 4 1 64
+./mergesort 100000000 4 1 64
+./mergesort 100000000 4 1 64
+./mergesort 100000000 4 1 64
+./mergesort 100000000 4 1 64
+./mergesort 100000000 4 1 64
+
+# 15th test
+./mergesort 1000000000 4 1 64
+./mergesort 1000000000 4 1 64
+./mergesort 1000000000 4 1 64
+./mergesort 1000000000 4 1 64
+./mergesort 1000000000 4 1 64
+./mergesort 1000000000 4 1 64
+./mergesort 1000000000 4 1 64
+./mergesort 1000000000 4 1 64
+./mergesort 1000000000 4 1 64
+./mergesort 1000000000 4 1 64
+
+# QUICKSORT
+# run the first test 10 times
+./mergesort 10000000 0 2 64
+./mergesort 10000000 0 2 64
+./mergesort 10000000 0 2 64
+./mergesort 10000000 0 2 64
+./mergesort 10000000 0 2 64
+./mergesort 10000000 0 2 64
+./mergesort 10000000 0 2 64
+./mergesort 10000000 0 2 64
+./mergesort 10000000 0 2 64
+./mergesort 10000000 0 2 64
+
+# 2nd test
+./mergesort 100000000 0 2 64
+./mergesort 100000000 0 2 64
+./mergesort 100000000 0 2 64
+./mergesort 100000000 0 2 64
+./mergesort 100000000 0 2 64
+./mergesort 100000000 0 2 64
+./mergesort 100000000 0 2 64
+./mergesort 100000000 0 2 64
+./mergesort 100000000 0 2 64
+./mergesort 100000000 0 2 64
+
+# 3rd test
+./mergesort 1000000000 0 2 64
+./mergesort 1000000000 0 2 64
+./mergesort 1000000000 0 2 64
+./mergesort 1000000000 0 2 64
+./mergesort 1000000000 0 2 64
+./mergesort 1000000000 0 2 64
+./mergesort 1000000000 0 2 64
+./mergesort 1000000000 0 2 64
+./mergesort 1000000000 0 2 64
+./mergesort 1000000000 0 2 64
+
+# 4th test
+./mergesort 10000000 1 2 64
+./mergesort 10000000 1 2 64
+./mergesort 10000000 1 2 64
+./mergesort 10000000 1 2 64
+./mergesort 10000000 1 2 64
+./mergesort 10000000 1 2 64
+./mergesort 10000000 1 2 64
+./mergesort 10000000 1 2 64
+./mergesort 10000000 1 2 64
+./mergesort 10000000 1 2 64
+
+# 5th test
+./mergesort 100000000 1 2 64
+./mergesort 100000000 1 2 64
+./mergesort 100000000 1 2 64
+./mergesort 100000000 1 2 64
+./mergesort 100000000 1 2 64
+./mergesort 100000000 1 2 64
+./mergesort 100000000 1 2 64
+./mergesort 100000000 1 2 64
+./mergesort 100000000 1 2 64
+./mergesort 100000000 1 2 64
+
+# 6th test
+./mergesort 1000000000 1 2 64
+./mergesort 1000000000 1 2 64
+./mergesort 1000000000 1 2 64
+./mergesort 1000000000 1 2 64
+./mergesort 1000000000 1 2 64
+./mergesort 1000000000 1 2 64
+./mergesort 1000000000 1 2 64
+./mergesort 1000000000 1 2 64
+./mergesort 1000000000 1 2 64
+./mergesort 1000000000 1 2 64
+
+# 7th test
+./mergesort 10000000 2 2 64
+./mergesort 10000000 2 2 64
+./mergesort 10000000 2 2 64
+./mergesort 10000000 2 2 64
+./mergesort 10000000 2 2 64
+./mergesort 10000000 2 2 64
+./mergesort 10000000 2 2 64
+./mergesort 10000000 2 2 64
+./mergesort 10000000 2 2 64
+./mergesort 10000000 2 2 64
+
+# 8th test
+./mergesort 100000000 2 2 64
+./mergesort 100000000 2 2 64
+./mergesort 100000000 2 2 64
+./mergesort 100000000 2 2 64
+./mergesort 100000000 2 2 64
+./mergesort 100000000 2 2 64
+./mergesort 100000000 2 2 64
+./mergesort 100000000 2 2 64
+./mergesort 100000000 2 2 64
+./mergesort 100000000 2 2 64
+
+# 9th test
+./mergesort 1000000000 2 2 64
+./mergesort 1000000000 2 2 64
+./mergesort 1000000000 2 2 64
+./mergesort 1000000000 2 2 64
+./mergesort 1000000000 2 2 64
+./mergesort 1000000000 2 2 64
+./mergesort 1000000000 2 2 64
+./mergesort 1000000000 2 2 64
+./mergesort 1000000000 2 2 64
+./mergesort 1000000000 2 2 64
+
+# 10th test
+./mergesort 10000000 3 2 64
+./mergesort 10000000 3 2 64
+./mergesort 10000000 3 2 64
+./mergesort 10000000 3 2 64
+./mergesort 10000000 3 2 64
+./mergesort 10000000 3 2 64
+./mergesort 10000000 3 2 64
+./mergesort 10000000 3 2 64
+./mergesort 10000000 3 2 64
+./mergesort 10000000 3 2 64
+
+# 11th test
+./mergesort 100000000 3 2 64
+./mergesort 100000000 3 2 64
+./mergesort 100000000 3 2 64
+./mergesort 100000000 3 2 64
+./mergesort 100000000 3 2 64
+./mergesort 100000000 3 2 64
+./mergesort 100000000 3 2 64
+./mergesort 100000000 3 2 64
+./mergesort 100000000 3 2 64
+./mergesort 100000000 3 2 64
+
+# 12th test
+./mergesort 1000000000 3 2 64
+./mergesort 1000000000 3 2 64
+./mergesort 1000000000 3 2 64
+./mergesort 1000000000 3 2 64
+./mergesort 1000000000 3 2 64
+./mergesort 1000000000 3 2 64
+./mergesort 1000000000 3 2 64
+./mergesort 1000000000 3 2 64
+./mergesort 1000000000 3 2 64
+./mergesort 1000000000 3 2 64
+
+# 13th test
+./mergesort 10000000 4 2 64
+./mergesort 10000000 4 2 64
+./mergesort 10000000 4 2 64
+./mergesort 10000000 4 2 64
+./mergesort 10000000 4 2 64
+./mergesort 10000000 4 2 64
+./mergesort 10000000 4 2 64
+./mergesort 10000000 4 2 64
+./mergesort 10000000 4 2 64
+./mergesort 10000000 4 2 64
+
+# 14th test
+./mergesort 100000000 4 2 64
+./mergesort 100000000 4 2 64
+./mergesort 100000000 4 2 64
+./mergesort 100000000 4 2 64
+./mergesort 100000000 4 2 64
+./mergesort 100000000 4 2 64
+./mergesort 100000000 4 2 64
+./mergesort 100000000 4 2 64
+./mergesort 100000000 4 2 64
+./mergesort 100000000 4 2 64
+
+# 15th test
+./mergesort 1000000000 4 2 64
+./mergesort 1000000000 4 2 64
+./mergesort 1000000000 4 2 64
+./mergesort 1000000000 4 2 64
+./mergesort 1000000000 4 2 64
+./mergesort 1000000000 4 2 64
+./mergesort 1000000000 4 2 64
+./mergesort 1000000000 4 2 64
+./mergesort 1000000000 4 2 64
+./mergesort 1000000000 4 2 64
